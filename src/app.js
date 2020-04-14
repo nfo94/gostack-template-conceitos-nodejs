e4c86995129b26a46c3e20e959a4929242afc34f
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2020-04-14 10:01:04 -0300

    Adding POST method for likes

--- src/app.js
+++ src/app.js
@@ -71,7 +71,19 @@ app.delete('/repositories/:id', (request, response) => {
 });
 
 app.post('/repositories/:id/like', (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exists' });
+  }
+
+  repositories[repositoryIndex].likes += 1;
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
